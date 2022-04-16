d19907b67cb35b27d1f784240c5b15d9d116e058
skalem <pimako2003@yandex.by>
2022-04-16 18:51:38 +0300

    Added container for tubes

---
+++ Models/TubePair.cpp
@@ -0,0 +1,23 @@
+#include "TubePair.h"
+
+TubePair::TubePair(int x, int y, int width, int screenHeight, int window) {
+    lowerRect = new QRect(x, y, width, screenHeight - y);
+    upperRect = new QRect(x, 0, width, y - window);
+}
+
+QRect *TubePair::getLower() {
+    return lowerRect;
+}
+
+QRect *TubePair::getUpper() {
+    return upperRect;
+}
+
+int TubePair::getX() {
+    return lowerRect->x();
+}
+
+void TubePair::moveLeft() {
+    lowerRect->moveLeft(lowerRect->x() - 1);
+    upperRect->moveLeft(upperRect->x() - 1);
+}
---
+++ Models/TubePair.h
@@ -0,0 +1,20 @@
+#ifndef FLAPPYBIRD_TUBEPAIR_H
+#define FLAPPYBIRD_TUBEPAIR_H
+
+#include <QRect>
+
+class TubePair {
+private:
+    QRect* lowerRect;
+    QRect* upperRect;
+public:
+    TubePair(int x, int y, int width, int screenHeight, int window);
+
+    QRect* getLower();
+    QRect* getUpper();
+    int getX();
+    void moveLeft();
+};
+
+
+#endif //FLAPPYBIRD_TUBEPAIR_H
