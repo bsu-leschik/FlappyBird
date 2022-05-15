c4bcb0095ca4df103a34c1ef5d652fef8f4f0a94
skalem <pimako2003@yandex.by>
2022-05-15 22:32:01 +0300

    Small fix

--- Controllers/ScoreController.cpp
+++ Controllers/ScoreController.cpp
@@ -4,28 +4,30 @@ ScoreController::ScoreController(int x, int y) {
     x_ = x;
     y_ = y;
 }
-void ScoreController::paint(QPainter* painter) {
+
+void ScoreController::paint(QPainter *painter) {
     QString scoreStr;
-    if (score <= 9){
+    if (score <= 9) {
         scoreStr = QString().fromStdString(std::to_string(score));
         painter->drawImage(QPoint(x_, y_),
                            QImage(folder + scoreStr));
     } else {
         std::vector<int> scores;
-        while(score != 0){
+        int tempScore = score;
+        while (score != 0) {
             scores.push_back(score % 10);
-            score /= 10;
+            score = (score - score % 10) / 10;
         }
+        score = tempScore;
 
         while (!scores.empty()) {
             scoreStr = QString().fromStdString(std::to_string(scores.back()));
-            painter->drawImage(QPoint(x_ - 30*scores.size(), y_ - 30*scores.size()),
+            painter->drawImage(QPoint(x_ - 30 * scores.size(), y_),
                                QImage(folder + scoreStr));
             scores.pop_back();
         }
 
     }
-
 
 
 }
